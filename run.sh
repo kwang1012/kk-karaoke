c4b7a20f7633aaee94d63c13bbbd418a83878780
Kai-Siang Wang <bruce1198@gmail.com>
2025-05-10 10:47:52 -0500

    add docker files

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# For backend
+redis-server --daemonize yes
+
+# Start FastAPI backend
+cd backend
+uvicorn main:app --host 0.0.0.0 --port 8000 &
+celery -A services.process_request.celery worker -l info --concurrency=1 &
+
+
+# Start Nginx (serving frontend)
+nginx -g 'daemon off;'
